976f6e38e37d86c2c021a2acb5f38b97fcc3e684
Mats Jensen <matsjensen@Matss-MacBook-Pro.local>
2016-04-18 22:00:31 +0200

    added some pictures, far from finished still

--- public/script.js
+++ public/script.js
@@ -4,6 +4,21 @@ window.onload = function() {
 $.getJSON('hvordan.json', function(data) {
   $get.html('');
 
+  //json to array decreasing
+  var arr = Object.keys(data.Articles).map(function(k) { return data.Articles[k] });
+  //sort array
+  arr.sort(function(a, b) {
+    return parseFloat(b.Views) - parseFloat(a.Views);
+  });
+  console.log(arr);
+
+  //json to array decreasing
+  var arr2 = Object.keys(data.Articles).map(function(k) { return data.Articles[k] });
+  //sort array
+  arr2.sort(function(a, b) {
+    return parseFloat(b.Date) - parseFloat(a.Date);
+  });
+
   $.each(data.Categories, function(key, val) {
     $('.cat').append('<p class="pcat" id=' + val.ID + '>' + val.Title + '</p>');
     $('.cat').append('<div class="seperator"></div>');
@@ -27,15 +42,54 @@ $.getJSON('hvordan.json', function(data) {
     }
   });
 
-/*
-  $.each(data.Articles, function(key, val) {
-    $('.articles').append('<p>' + val.Title + '</p>');
-  });*/
+  $.each(arr2, function(key, val) {
+    if(val.Image === 'Car1.jpg') {
+      $('.articles').append('<div class="otherimg"><img class="other" src="./images/Car1.jpg" />' +
+      '<h3 class="title">' + val.Title + '</h3><p class="desc">' + val.Intro + '</p></div>');
+    }
+    else if(val.Image === 'Car3.jpg') {
+      $('.articles').append('<div class="otherimg"><img class="other" src="./images/Car3.jpg" />' +
+      '<h3 class="title">' + val.Title + '</h3><p class="desc">' + val.Intro + '</p></div>');
+    }
+  });
+  $.each(arr, function(key, val) {
+    //main article, newest first
+    /*
+    else if(val.Image === 'Appletree.jpg') {
+      $('.sidediv').append('<div class="imgs"><img class="sideimgs" src="./images/Appletree.jpg" /></div>' +
+      '<h3>' + val.Title + '</h3></div>');
+    }
+    */
+
+    if(val.Image === 'Paintingfloor.jpg') {
+      $('.sidediv').append('<div class="imgs"><img class="other" src="./images/Paintingfloor.jpg" /><h3>'+val.Title+'</h3></div>');
+    }
+    else if(val.Image === 'Car3.jpg') {
+      $('.sidediv').append('<div class="imgs"><img class="other" src="./images/Car3.jpg" /><h3>'+val.Title+'</h3></div>');
+    }
+    else if(val.Image === 'Boat1.jpg') {
+      $('.sidediv').append('<div class="imgs"><img class="other" src="./images/Boat1.jpg" /><h3>'+val.Title+'</h3></div>');
+    }
+    else if(val.Image === 'Boat2.jpg') {
+      $('.sidediv').append('<div class="imgs"><img class="other" src="./images/Boat2.jpg" /><h3>'+val.Title+'</h3></div>');
+    }
+    else if(val.Image === 'Appletree.jpg') {
+      $('.sidediv').append('<div class="imgs"><img class="other" src="./images/Appletree.jpg" /><h3>'+val.Title+'</h3></div>');
+    }
+
+  });
 });
 
 $(document).ready(function(){
 	$('#hamburger').click(function(){
+    $('#hamburger').css('display', 'none');
+    $('#exit').css('display', 'block');
 		$('.cat').slideToggle();
 	});
+  $('#exit').click(function() {
+    $('#exit').css('display', 'none');
+    $('#hamburger').css('display', 'block');
+    $('.cat').slideToggle();
+  });
 });
 };
